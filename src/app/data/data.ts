a1df3748e8c8d6a8198c51325853d1e28f20e1ba
GustavoCR021 <gustavoc1922@gmail.com>
2024-09-19 21:39:55 -0300

    feat: test data.ts

---
+++ src/app/data/data.ts
@@ -0,0 +1,15 @@
+export const products = [
+  {
+    id: '1',
+    name: 'Produto 1',
+    description: 'Descrição do Produto 1',
+    price: 29.99,
+  },
+  {
+    id: '2',
+    name: 'Produto 2',
+    description: 'Descrição do Produto 2',
+    price: 49.99,
+  },
+  // Adicione mais produtos conforme necessário
+];
